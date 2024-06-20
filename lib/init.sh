c776a6f3a2fcaef766445fafa58a785764318054
Peter Toth <tothpeter08@gmail.com>
2024-06-21 01:30:27 +0400

    Unload previously loaded variables, aliases, functions
    This restores the original variables, aliases, functions after leaving
    a directory.

--- lib/init.sh
+++ lib/init.sh
@@ -29,5 +29,23 @@ load_local_config() {
 }
 
 stash_local_config() {
-  autostash var_1
+  local file_to_parse=".local-sherpa"
+
+  # Stash variables
+  grep -o '^\s*export\s\+\([^=]\+\)' "$file_to_parse" | while read -r line; do
+    variable_name=$(echo "$line" | awk '{print $2}')
+    autostash "$variable_name"
+  done
+
+  # Stash aliases
+  grep -o '^\s*alias\s\+\([^=]\+\)' "$file_to_parse" | while read -r line; do
+    alias=$(echo "$line" | awk '{print $2}')
+    autostash $alias
+  done
+
+  # Stash functions
+  grep -o '^\s*\([^()]\+\)\s*()' "$file_to_parse" | while read -r line; do
+    function_name=$(echo "$line" | sed 's/^\s*//; s/\s*()//')
+    autostash "$function_name"
+  done
 }
