3f593318e795ba0ac7337f2ce7edeed4f32fd0c7
Peter Toth <tothpeter08@gmail.com>
2024-06-20 21:30:16 +0400

    First fake working version

--- lib/init.sh
+++ lib/init.sh
@@ -1,7 +1,13 @@
+source ../vendor/smartcd/arrays
+source ../vendor/smartcd/smartcd
+source ../vendor/smartcd/varstash
+
 function chpwd() {
   if [[ -n $OLDPWD && $PWD != $OLDPWD ]]; then
-    echo "Left: $OLDPWD"
-    echo "Entered: $PWD"
+    varstash_dir=$OLDPWD
+    unload_previous_local_config $OLDPWD
+    varstash_dir=$PWD
+    load_local_config $PWD
   fi
   export OLDPWD=$PWD
 }
@@ -9,11 +15,20 @@ function chpwd() {
 autoload -U add-zsh-hook
 add-zsh-hook chpwd chpwd
 
-# Worked
-# # autoload -U add-zsh-hook
+unload_previous_local_config() {
+  unstash var_1
+  # autounstash
+  echo "Config unloaded."
+}
 
-# _smartcd_hook() {
-#   echo 'AAAAA'
-# }
+load_local_config() {
+  if [ -f .local-sherpa ]; then
+    stash_local_config
+    source .local-sherpa
+    echo "Config loaded."
+  fi
+}
 
-# chpwd_functions=(${chpwd_functions[@]} "_smartcd_hook")
+stash_local_config() {
+  stash var_1
+}
