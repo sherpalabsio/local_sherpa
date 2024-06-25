1b13577c0811af7617a92374e9c6026cf4286f66
Peter Toth <tothpeter08@gmail.com>
2024-06-26 01:05:49 +0400

    Run the whole test suite and write a result message

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-zsh tests/loading_and_unloading_config.sh
+_test_files=(tests/*_test.sh)
+_all_tests_passed=true
+
+for file in "${_test_files[@]}" ; do
+  echo "Running $file"
+  zsh $file || _all_tests_passed=false
+done
+
+if $_all_tests_passed; then
+  echo -e "\033[32mAll tests passed successfully!\033[0m"
+else
+  echo -e "\033[31mSome tests failed.\033[0m"
+fi
