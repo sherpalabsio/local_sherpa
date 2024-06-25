a1c4196c321be80a554becf4d78bb622ee6e630d
Peter Toth <tothpeter08@gmail.com>
2024-06-25 22:18:32 +0400

    Refactor: use a better test matcher

--- tests/loading_and_unloading_config.sh
+++ tests/loading_and_unloading_config.sh
@@ -13,33 +13,33 @@ cd tests/playground
 source .bash_profile
 
 echo "==== Original env ===="
-like "$var_1" "ORIGINAL VAR"
-like "$(alias_1)" "ORIGINAL ALIAS"
-like "$(function_1)" "ORIGINAL FUNCTION"
+is $var_1 "ORIGINAL VAR"
+is "$(alias_1)" "ORIGINAL ALIAS"
+is "$(function_1)" "ORIGINAL FUNCTION"
 
 # Load the Project 1 env
 echo "\n==== Project 1 env ===="
 cd project_1
 trust_local_sherpa
-like "$var_1" "VAR PROJECT 1"
-like "$(alias_1)" "ALIAS PROJECT 1"
-like "$(function_1)" "FUNCTION PROJECT 1"
+is $var_1 "VAR PROJECT 1"
+is "$(alias_1)" "ALIAS PROJECT 1"
+is "$(function_1)" "FUNCTION PROJECT 1"
 
 # Unload the Project 1 env
 # Load the Project 2 env
 echo "\n==== Project 2 env ===="
 cd ../project_2
 trust_local_sherpa
-like "$var_1" "VAR PROJECT 2"
-like "$(alias_1)" "ALIAS PROJECT 2"
-like "$(function_1)" "FUNCTION PROJECT 2"
+is $var_1 "VAR PROJECT 2"
+is "$(alias_1)" "ALIAS PROJECT 2"
+is "$(function_1)" "FUNCTION PROJECT 2"
 
 # Unload the Project 2 env
 echo "\n==== Original env again ===="
 cd ..
-like "$var_1" "ORIGINAL VAR"
-like "$(alias_1)" "ORIGINAL ALIAS"
-like "$(function_1)" "ORIGINAL FUNCTION"
+is "$var_1" "ORIGINAL VAR"
+is "$(alias_1)" "ORIGINAL ALIAS"
+is "$(function_1)" "ORIGINAL FUNCTION"
 
 # Tear down
 rm -rf $SHERPA_CHECKSUM_DIR
