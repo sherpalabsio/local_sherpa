43d507de2c00ea5bfbc185a073757849da81e20f
Peter Toth <tothpeter08@gmail.com>
2024-06-25 21:52:18 +0400

    Refactor

--- tests/loading_and_unloading_config.sh
+++ tests/loading_and_unloading_config.sh
@@ -1,24 +1,23 @@
 source lib/init.zsh
 
-SHERPA_CHECKSUM_DIR="$SHERPA_PATH/tests/playground/local_sherpa_checksums"
-
 # Load testing library
 source tests/lib/tap-functions
+
+SHERPA_CHECKSUM_DIR="$SHERPA_PATH/tests/playground/local_sherpa_checksums"
 
 plan_tests 12
 
 # Start the test
 cd tests/playground
 # Load the global env
+source .bash_profile
 
 echo "==== Original env ===="
-
-source .bash_profile
 like "$var_1" "ORIGINAL VAR"
 like "$(alias_1)" "ORIGINAL ALIAS"
 like "$(function_1)" "ORIGINAL FUNCTION"
 
-# Load the project env
+# Load the Project 1 env
 echo "\n==== Project 1 env ===="
 cd project_1
 trust_local_sherpa
@@ -26,6 +25,8 @@ like "$var_1" "VAR PROJECT 1"
 like "$(alias_1)" "ALIAS PROJECT 1"
 like "$(function_1)" "FUNCTION PROJECT 1"
 
+# Unload the Project 1 env
+# Load the Project 2 env
 echo "\n==== Project 2 env ===="
 cd ../project_2
 trust_local_sherpa
@@ -33,6 +34,7 @@ like "$var_1" "VAR PROJECT 2"
 like "$(alias_1)" "ALIAS PROJECT 2"
 like "$(function_1)" "FUNCTION PROJECT 2"
 
+# Unload the Project 2 env
 echo "\n==== Original env again ===="
 cd ..
 like "$var_1" "ORIGINAL VAR"
