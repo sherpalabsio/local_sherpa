e17fdff6d1a3e06d77fcd7eccee85522c69adc9c
Peter Toth <tothpeter08@gmail.com>
2024-07-01 22:48:12 +0400

    Cleanup hooking into cd in Bash

--- lib/init.sh
+++ lib/init.sh
@@ -38,43 +38,18 @@ if [ -n "$ZSH_VERSION" ]; then
   add-zsh-hook chpwd sherpa_chpwd_handler
 else
   # BASH
-  : # Do nothing
-  # Todo: add CHPWD_COMMAND
+  _sherpa_chpwd_hook() {
+    # run commands in CHPWD_COMMAND variable on dir change
+    if [[ "$PREVPWD" != "$PWD" ]]; then
+      alert_sherpa_we_changed_dir
+    fi
+    # refresh last working dir record
+    export PREVPWD="$PWD"
+  }
+
+  # add `;` after _sherpa_chpwd_hook if PROMPT_COMMAND is not empty
+  PROMPT_COMMAND="_sherpa_chpwd_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
 fi
 
 # When loading the shell, we need to make sure that the sherpa is doing its job
 load_local_env
-
-
-
-
-
-
-
-
-
-
-typeset -g CHPWD_COMMAND=""
-
-_chpwd_hook() {
-  # shopt -s nullglob
-
-  local f
-
-  # run commands in CHPWD_COMMAND variable on dir change
-  if [[ "$PREVPWD" != "$PWD" ]]; then
-    local IFS=$';'
-    for f in $CHPWD_COMMAND; do
-      "$f"
-    done
-    unset IFS
-  fi
-  # refresh last working dir record
-  export PREVPWD="$PWD"
-}
-
-# add `;` after _chpwd_hook if PROMPT_COMMAND is not empty
-PROMPT_COMMAND="_chpwd_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
-
-# append the command into CHPWD_COMMAND
-CHPWD_COMMAND="${CHPWD_COMMAND:+$CHPWD_COMMAND;}alert_sherpa_we_changed_dir"
