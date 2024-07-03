c26c156b3f997663b52bda11266f865e9aa517cd
Peter Toth <tothpeter08@gmail.com>
2024-07-03 10:49:32 +0400

    Start working on supportting smarter env load and unload
    Don't unload env when cd into a subfolder.
    Don't reload already loaded env when cd back.
    Load multiple envs on the same path branch and unload them in the right order.

--- lib/sherpa.sh
+++ lib/sherpa.sh
@@ -60,15 +60,32 @@ alert_sherpa_we_changed_dir() {
   # Skip if Sherpa is not enabled
   [ -z "$SHERPA_ENABLED" ] && return
   log_debug "Directory changed."
-  unload_previously_loaded_env
+  unload_inactive_envs
   load_local_env
 }
 
-unload_previously_loaded_env() {
-  log_debug "Unloading env."
-  varstash_dir="$OLDPWD"
-  autounstash
+PATHS_WHERE_LOCAL_ENV_WAS_LOADED=()
+
+# It unloads the local envs that were loaded in the previous directories that
+# we exited from.
+unload_inactive_envs() {
+  local loaded_paths=()
+
+  for loaded_path in "${PATHS_WHERE_LOCAL_ENV_WAS_LOADED[@]}"; do
+    # Select paths not beginning with the current directory
+    if [[ $(pwd) != "$loaded_path"* ]]; then
+      log_debug "Unload env: $loaded_path"
+      varstash_dir="$loaded_path"
+      autounstash
+    else
+      loaded_paths+=("$loaded_path")
+    fi
+  done
+
+  PATHS_WHERE_LOCAL_ENV_WAS_LOADED=("${loaded_paths[@]}")
 }
+
+# todo: unload_currently_loaded_env -> unload_all_envs
 
 unload_currently_loaded_env() {
   varstash_dir="$PWD"
@@ -83,12 +100,29 @@ load_local_env() {
   # Skip if there is no local env file
   [ -f .local-sherpa ] || { log_debug "No local env file"; return; }
 
+  # Skip if the env was already loaded
+  was_env_loaded && { log_debug "Local env already loaded"; return; }
+
   # Skip if the local env file is not trusted
   verify_trust || return;
 
   stash_local_env
-  log_debug "Loaded local env"
+  log_debug "Load local env"
   source .local-sherpa
+  # Append the current directory to the list. This is needed to unload the envs
+  # in the right order when we change directories. The root directory should be
+  # the last one to unload.
+  PATHS_WHERE_LOCAL_ENV_WAS_LOADED=($(pwd) "${PATHS_WHERE_LOCAL_ENV_WAS_LOADED[@]}")
+}
+
+was_env_loaded() {
+  for loaded_path in "${PATHS_WHERE_LOCAL_ENV_WAS_LOADED[@]}"; do
+    if [[ "$loaded_path" == $(pwd) ]]; then
+      return 0
+    fi
+  done
+
+  return 1
 }
 
 stash_local_env() {
