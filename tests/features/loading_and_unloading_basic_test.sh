bbbefa202c4a87fa278bafadd9e457219b547431
Peter Toth <tothpeter08@gmail.com>
2024-07-07 01:53:40 +0400

    Add missing line breaks in test prints

--- tests/features/loading_and_unloading_basic_test.sh
+++ tests/features/loading_and_unloading_basic_test.sh
@@ -11,7 +11,7 @@ is "$(alias_1)" "GLOBAL ALIAS"
 is "$(function_1)" "GLOBAL FUNCTION"
 
 # Load the Project 1 env
-printf "\n==== Project 1 env ===="
+printf "\n==== Project 1 env ====\n"
 cd project_1
 sherpa trust
 is "$var_1" "LOCAL VAR PROJECT 1"
@@ -20,7 +20,7 @@ is "$(function_1)" "LOCAL FUNCTION PROJECT 1"
 
 # Unload the Project 1 env
 # Load the Project 2 env
-printf "\n==== Project 2 env ===="
+printf "\n==== Project 2 env ====\n"
 cd ../project_2
 sherpa trust
 is "$var_1" "LOCAL VAR PROJECT 2"
@@ -28,7 +28,7 @@ is "$(alias_1)" "LOCAL ALIAS PROJECT 2"
 is "$(function_1)" "LOCAL FUNCTION PROJECT 2"
 
 # Unload the Project 2 env
-printf "\n==== Original env again ===="
+printf "\n==== Original env again ====\n"
 cd ..
 is "$var_1" "GLOBAL VAR"
 is "$(alias_1)" "GLOBAL ALIAS"
