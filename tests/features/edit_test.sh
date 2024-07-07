aac2dfb775a61f3cd4374293d4a22d0bf0d05feb
Peter Toth <tothpeter08@gmail.com>
2024-07-07 23:50:17 +0400

    Replace sed with perl in the edit feature test
    Sed doesn't work the same in BSD (macOS) and GNU Linux.
    In macOS it needs an extra param which crashes it in Linux.

--- tests/features/edit_test.sh
+++ tests/features/edit_test.sh
@@ -8,7 +8,7 @@ is "$var_1" "LOCAL VAR PROJECT 1" "The local env is loaded"
 
 # When editing the local env file with Sherpa
 # It opens the default editor
-EDITOR="sed -i '' '1s/LOCAL VAR PROJECT 1/LOCAL VAR PROJECT 8/'" sherpa edit > /dev/null
+EDITOR="perl -pi -e 's/LOCAL VAR PROJECT 1/LOCAL VAR PROJECT 8/'" sherpa edit  > /dev/null
 
 # When the user saves and closes the local env file
 # Sherpa auto trusts the local env file and loads it
