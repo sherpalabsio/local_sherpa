ee7f5ec5eccfbebe35e66998f73235014c467793
Peter Toth <tothpeter08@gmail.com>
2024-07-15 19:39:42 +0400

    Refactor parsing local env file
    Make its helpers private and its local variables readonly.

--- lib/local_env_file_parser.sh
+++ lib/local_env_file_parser.sh
@@ -1,47 +1,45 @@
-# It returns a list of: variable, function and alias names
+# It returns a list that contains variable, function and alias names
 _local_sherpa_parse_local_env_file() {
-  _local_sherpa_fetch_variable_names
-  _local_sherpa_fetch_aliase_names
-  _local_sherpa_fetch_function_names
-}
+  fetch_variable_names() {
+    local -r filter_pattern='^[[:space:]]*export[[:space:]]+[[:alnum:]_]+'
+    local variable_names
+    # Cleanup:
+    # export var_1 -> var_1
+    variable_names=$(grep -oE "$filter_pattern" "$SHERPA_ENV_FILENAME" | awk '{print $2}')
 
-_local_sherpa_fetch_variable_names() {
-  local filter_pattern='^[[:space:]]*export[[:space:]]+[[:alnum:]_]+'
-  # Cleanup:
-  # export var_1 -> var_1
-  local variable_names
-  variable_names=$(grep -oE "$filter_pattern" "$SHERPA_ENV_FILENAME" | awk '{print $2}')
+    if [ -n "$variable_names" ]; then
+      echo "$variable_names"
+    fi
+  }
 
-  if [ -n "$variable_names" ]; then
-    echo "$variable_names"
-  fi
-}
+  fetch_aliase_names() {
+    local -r filter_pattern='^[[:space:]]*alias[[:space:]]'
+    local alias_names
+    # Cleanup:
+    # alias alias_1=... -> alias_1
+    alias_names=$(grep -E "$filter_pattern" "$SHERPA_ENV_FILENAME" | awk -F'[ =]' '{print $2}')
 
-_local_sherpa_fetch_aliase_names() {
-  local filter_pattern
-  filter_pattern='^[[:space:]]*alias[[:space:]]'
-  # Cleanup:
-  # alias alias_1=... -> alias_1
-  local alias_names
-  alias_names=$(grep -E "$filter_pattern" "$SHERPA_ENV_FILENAME" | awk -F'[ =]' '{print $2}')
+    if [ -n "$alias_names" ]; then
+      echo "$alias_names"
+    fi
+  }
 
-  if [ -n "$alias_names" ]; then
-    echo "$alias_names"
-  fi
-}
+  fetch_function_names() {
+    local -r filter_pattern='^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)'
+    local function_names
+    # Cleanup:
+    # function_1() -> function_1
+    # function function_2() -> function_2() -> function_2
+    function_names=$(grep -oE "$filter_pattern" "$SHERPA_ENV_FILENAME" | \
+                    sed 's/function //' | \
+                    sed 's/()//')
 
-_local_sherpa_fetch_function_names() {
-  local filter_pattern
-  filter_pattern='^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)'
-  # Cleanup:
-  # function_1() -> function_1
-  # function function_2() -> function_2() -> function_2
-  local function_names
-  function_names=$(grep -oE "$filter_pattern" "$SHERPA_ENV_FILENAME" | \
-                   sed 's/function //' | \
-                   sed 's/()//')
+    if [ -n "$function_names" ]; then
+      echo "$function_names"
+    fi
+  }
 
-  if [ -n "$function_names" ]; then
-    echo "$function_names"
-  fi
+  fetch_variable_names
+  fetch_aliase_names
+  fetch_function_names
 }
