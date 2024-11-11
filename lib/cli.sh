8f9d344fc758604581ba58f885af91330100da8c
Peter Toth <tothpeter08@gmail.com>
2024-11-11 17:11:57 +0100

    Stop the execution when the env file is not created afer editting the environment
    It happens when the user closes the new env file without saving it the very first time. It crashed the app.

--- lib/cli.sh
+++ lib/cli.sh
@@ -71,6 +71,9 @@ _sherpa_cli_edit() {
     eval "$EDITOR $SHERPA_ENV_FILENAME"
   fi
 
+  # The user cancelled the init
+  [ -f "$SHERPA_ENV_FILENAME" ] || return 1
+
   _sherpa_trust_current_dir &&
     _sherpa_unload_env_of_current_dir &&
     _sherpa_load_env_from_current_dir 2> /dev/null
