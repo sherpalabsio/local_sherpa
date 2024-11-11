0ed82a99f3bbf9b14d658cd6d3125aff582d56dc
Peter Toth <tothpeter08@gmail.com>
2024-11-11 17:12:02 +0100

    Fix removing the first item from an array with only one item in Zsh
    "$array_name[@]" part was interpreted as one command and it broke the generated string for the eval

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,6 +1,6 @@
 _sherpa_utils::array::remove_first_element() {
   local -r array_name=$1
 
-  # shellcheck disable=SC1087
-  eval "$array_name=(\"\${$array_name[@]:1}\")"
+  # shellcheck disable=SC1087,SC2116,SC2086
+  eval "$array_name=(\"\${$(echo $array_name)[@]:1}\")"
 }
---
+++ tests/lib/utils_test.sh
@@ -0,0 +1,17 @@
+source tests/support/test_helper.sh
+source "$SHERPA_DIR/lib/utils.sh"
+
+# 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+#                              remove_first_element
+# 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+# ==============================================================================
+# ++++ It removes the first item from the given array
+
+_my_list=(1)
+
+_sherpa_utils::array::remove_first_element "_my_list"
+
+assert_equal "${#_my_list[@]}" "0"
+
+unset _my_list
