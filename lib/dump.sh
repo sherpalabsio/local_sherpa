7fc22823712c830db2ef4a6f91e56913670279b5
Peter Toth <tothpeter08@gmail.com>
2025-02-15 23:54:08 +0700

    Sanitize variables in the env dump

--- lib/dump.sh
+++ lib/dump.sh
@@ -1,3 +1,10 @@
 _sherpa_dump_current_env() {
-  cat "$SHERPA_ENV_FILENAME" > "$SHERPA_ENV_FILENAME.example"
+  _sherpa_dump_current_env__sanitize > "$SHERPA_ENV_FILENAME.example"
+}
+
+_sherpa_dump_current_env__sanitize() {
+  # shellcheck disable=SC2002
+  cat "$SHERPA_ENV_FILENAME" |
+    sed '/^export [^[:space:]]*=/s/=.*/=/' |
+    sed '/^[^[:space:]]*=/s/=.*/=/'
 }
--- tests/features/dump_test.sh
+++ tests/features/dump_test.sh
@@ -12,10 +12,48 @@ stub_env_file
 # ==============================================================================
 # ++++ It dumps the current env file into an example file
 
-overwrite_env_file "export VAR_2=\"local var 1\""
+cat << EOF | overwrite_env_file
+alias alias_name="command"
+
+function_name() {
+  inside_var="value"
+}
+EOF
+
+expected_env_file=$(
+                    cat << EOF
+alias alias_name="command"
+
+function_name() {
+  inside_var="value"
+}
+EOF
+)
 
 sherpa dump
 
 actual_env_file=$(cat "$SHERPA_ENV_FILENAME.example")
 
-assert_equal "$actual_env_file" "export VAR_2=\"local var 1\""
+assert_equal "$actual_env_file" "$expected_env_file"
+
+# ==============================================================================
+# ++++ It sanitizes the exported variables
+
+overwrite_env_file "export VAR_1=\"local var 1\""
+
+sherpa dump
+
+actual_env_file=$(cat "$SHERPA_ENV_FILENAME.example")
+
+assert_equal "$actual_env_file" "export VAR_1="
+
+# ==============================================================================
+# ++++ It sanitizes the non-exported variables
+
+overwrite_env_file "VAR_1=\"local var 1\""
+
+sherpa dump
+
+actual_env_file=$(cat "$SHERPA_ENV_FILENAME.example")
+
+assert_equal "$actual_env_file" "VAR_1="
