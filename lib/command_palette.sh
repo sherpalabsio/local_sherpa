ad16faa216eb487d69722ff48042ae19c7bda709
Peter Toth <tothpeter08@gmail.com>
2025-03-23 13:36:08 +0700

    Fetch the names of the loaded shell entities from the cache when opening the command palette

--- lib/command_palette.sh
+++ lib/command_palette.sh
@@ -37,22 +37,26 @@ _sherpa_command_palette() {
   fi
 }
 
+__sherpa_command_palette__load_local_variables() {
+  if [ -n "$ZSH_VERSION" ]; then
+    echo "$SHERPA_STATUS_INFO__VARS"
+  else
+    echo "${SHERPA_STATUS_INFO__VARS[@]}"
+  fi
+}
+
 __sherpa_command_palette__load_local_aliases() {
-  unalias -a
-
-  # shellcheck disable=SC1090
-  source "$SHERPA_ENV_FILENAME" &> /dev/null
-
-  compgen -a
+  if [ -n "$ZSH_VERSION" ]; then
+    echo "$SHERPA_STATUS_INFO__ALIASES"
+  else
+    echo "${SHERPA_STATUS_INFO__ALIASES[@]}"
+  fi
 }
 
 __sherpa_command_palette__load_local_functions() {
-  local -r filter_pattern="^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)"
-
-  # Cleanup:
-  # "function_1()" -> "function_1"
-  # "function function_2()" -> "function_2()" -> "function_2"
-  grep -oE "$filter_pattern" "$SHERPA_ENV_FILENAME" |
-    sed "s/function //" |
-    sed "s/()//"
+  if [ -n "$ZSH_VERSION" ]; then
+    echo "$SHERPA_STATUS_INFO__FUNCTIONS"
+  else
+    echo "${SHERPA_STATUS_INFO__FUNCTIONS[@]}"
+  fi
 }
